ddd2aebf9314bdbc9fcb03ef7d39a8a6378ca6e7
Misty Shaine Niones <125946850+msn02@users.noreply.github.com>
2026-04-04 14:19:49 +0800

    chore: updated About Me

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -3,12 +3,12 @@ export default function AboutMe() {
     <div>
       <h1 className="text-2xl font-bold text-gray-900 dark:text-white">About Me</h1>
       <p className="text-gray-700 dark:text-gray-300 mt-4">
-        Hi! I'm Shaine, a UI/UX designer and frontend developer based in Albay, Philippines. I specialize in UI/UX design and web development. Currently working as a Junior Programmer at Bicol University while also tutoring students in programming.      </p>
+        Hi! I'm Shaine, a UI/UX designer and frontend developer based in Albay, Philippines. I specialize in UI/UX design and web development. Currently working as a Junior Programmer at Bicol University.</p>
       <p className="text-gray-700 dark:text-gray-300 mt-4">
-        With a sharp eye for design and a drive to build, I develop prototypes that merge aesthetics with functionality. My journey has been shaped by hands-on system assessments, prototyping, and collaborative work. Right now I'm focused on expanding my skills in frontend development. 
+        I develop prototypes that merge aesthetics with functionality. My experience includes hands-on experience in system assessments, prototyping, and collaborative work across real projects. Right now, I'm exploring new tools in UI/UX design, while on the development side, I'm diving into React JS and expanding my frontend stack.
       </p>
       <p className="text-gray-700 dark:text-gray-300 mt-4">
-        My goal is to grow into roles that blend technical expertise with creative problem-solving and contribute to projects that make a real impact.
+        My goal is to grow into roles that blend technical skills with problem-solving and contribute to projects that make a real impact.
       </p>
     </div>
     );
